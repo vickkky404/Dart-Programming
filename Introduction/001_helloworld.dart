5f2536de3ffdb0d76a020bac4f847a977b6d0adf
nalin <nalinikantbastia@gmail.com>
2026-07-24 13:20:22 +0530

    cmnts

--- Introduction/001_helloworld.dart
+++ Introduction/001_helloworld.dart
@@ -1,3 +1,10 @@
 void main() {
   print("this is dart programming language");
 }
+
+// the execution starts from the main() function.\
+// the void means it will not return any value.
+
+// the print() function is a predefined function that is used to print the output statement.
+
+
