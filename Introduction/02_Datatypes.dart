09083d274454f638a2f65aef924c3a338b579ecf
nalin <nalinikantbastia@gmail.com>
2026-07-24 13:25:17 +0530

    concept of types of values in dart programming langauge.

---
+++ Introduction/02_Datatypes.dart
@@ -0,0 +1,19 @@
+//numbers , string , boolean , list , sets , maps ,  runes , symbols
+
+// number = int , double..
+
+void main() {
+  int a = 10;
+  print(a);
+
+  double b = 10.5;
+  print(b);
+
+  print(a + b); // addition
+
+  String name = 'Bob';
+  print(name);
+
+  bool isTrue = true;
+  print(isTrue);
+}
