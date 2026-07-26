4cbd01e8ca27a5146d74b510c9d979ca6ee55b2d
nalin <nalinikantbastia@gmail.com>
2026-07-26 20:40:26 +0530

    fundamentals

---
+++ Introduction/01_Arithmetic.dart
@@ -0,0 +1,27 @@
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter first number: ");
+//   double a = double.parse(stdin.readLineSync()!);
+
+//   stdout.write("Enter second number: ");
+//   double b = double.parse(stdin.readLineSync()!);
+
+
+//   print("sum of ${a + b }");  
+//   print("Difference: ${a - b}");
+//   print("Product: ${a * b}");
+//   print("Quotient: ${a / b}");
+//   print("Remainder: ${a % b}");
+// }
+
+
+void main() {
+     double a = 10;
+     double b = 3;
+     print("sum of ${a + b}");
+     print("Difference: ${a - b}");
+     print("Product: ${a * b}");
+     print("Quotient: ${a / b}");
+     print("Remainder: ${a % b}");
+   }
