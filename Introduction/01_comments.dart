bf098359d1e6523980226515f4952548b2a395a5
nalin <nalinikantbastia@gmail.com>
2026-07-26 17:17:50 +0530

    logic building

---
+++ Introduction/01_comments.dart
@@ -0,0 +1,13 @@
+void main() {
+  //Three types of comments in Dart:
+  //1. Single line comment
+  /* 2. Multi-line comment
+     This is a multi-line comment that can span multiple lines.
+  */
+
+  // 3. Documentation comment
+  /// This is a documentation comment.
+  /// It is used to generate documentation for the code.
+  /// You can use documentation comments to describe classes, methods, and variables.
+  /// You can also use documentation comments to provide examples of how to use the code.
+}
